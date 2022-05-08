cc1a1c051a80f5ce27a2108379bdf1fcf1db3887
Wagon <95537914+MeetmyWagon23@users.noreply.github.com>
2022-05-08 11:54:30 -0400

    Initial commit

---
+++ src/main/java/com/meetmywagon23/projectdnd/ProjectDnD.java
@@ -0,0 +1,10 @@
+package com.meetmywagon23.projectdnd;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(ProjectDnD.MODID)
+public class ProjectDnD {
+	public static final String MODID = "projectdnd";
+	
+	
+}
