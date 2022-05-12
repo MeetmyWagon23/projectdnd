3e6d0073e9f81556d5ffefa789b37ed0fee97520
Wagon <95537914+MeetmyWagon23@users.noreply.github.com>
2022-05-12 17:54:46 -0400

    added coins
    do not properly hold a stack value of 10000. will look into smaller values.

--- src/main/java/com/meetmywagon23/projectdnd/ProjectDnD.java
+++ src/main/java/com/meetmywagon23/projectdnd/ProjectDnD.java
@@ -1,10 +1,17 @@
 package com.meetmywagon23.projectdnd;
 
+import com.meetmywagon23.projectdnd.core.init.iteminit;
+
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(ProjectDnD.MODID)
 public class ProjectDnD {
 	public static final String MODID = "projectdnd";
 	
-	
+	public ProjectDnD() {
+		var bus = FMLJavaModLoadingContext.get().getModEventBus();
+		
+		iteminit.ITEMS.register(bus);
+	}
 }
